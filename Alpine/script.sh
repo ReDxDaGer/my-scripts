9eb0018006a75b08cb19767132cddf6d4e248104
ReDxDaGer <yashmehrotra45@gmail.com>
2024-03-03 22:59:29 +0530

    Added the folder for alpine based distros

---
+++ Alpine/script.sh
@@ -0,0 +1,102 @@
+#!/bin/ash
+
+# Check if git is installed or not
+if ! command -v git &> /dev/null; then
+  echo "Git is not installed, installing git ..."
+  apk update
+  apk add git
+
+  # Check if git install was a success or not
+  if ! command -v git &> /dev/null; then
+    echo "Failed to install git, check your internet maybe!"
+    exit 1
+  else
+    echo "Git installed successfully"
+  fi
+fi
+
+echo "Installing CURL"
+# Install curl
+apk add curl
+
+echo "Installing Rust 🦀"
+# Install rust
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Install all other packages (essential)
+apk add build-base brave spotify-client discord neofetch nano code htop font-symbola noto-fonts-cjk noto-fonts-emoji fzf kcalc okular
+
+echo "All the packages basically required are installed !!"
+
+# Installing the starship bash config 
+curl -fsSL https://starship.rs/install.sh | bash
+echo 'eval "$(starship init bash)"' >> ~/.profile
+
+echo "Starship config has been added to your konsole !!"
+
+echo "Now installing Volta and node latest"
+
+read -p "Do you want to install Node.js using Volta? (yes/no): " choice
+
+case "$choice" in
+  yes|Yes|YES)
+    echo "Installing Node.js using Volta..."
+    curl https://get.volta.sh | bash
+    source ~/.profile
+    volta install node
+    ;;
+  no|No|NO)
+    echo "Skipping Node.js installation."
+    ;;
+  *)
+    echo "Invalid choice, please enter 'yes' or 'no'."
+    ;;
+esac
+
+read -p "Do you want to install Java using SDKman? (yes/no): " choice
+
+case "$choice" in
+  yes|Yes|YES)
+    echo "Now installing SDKMAN and Java version 17.0.9 of microsoft "
+    # Install java latest using SDKMAN
+    curl -s "https://get.sdkman.io" | bash
+    source ~/.profile
+    sdk install java 17.0.9-ms
+    ;;
+  no|No|NO)
+    echo "Skipping Java installation."
+    ;;
+  *)
+    echo "Invalid choice, please enter 'yes' or 'no'."
+    ;;
+esac
+
+echo "Java latest installed"
+
+echo "Now Installing Miniconda"
+
+# Install miniconda (Assuming x86 system, again feel free to modify the script)
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+
+echo "Restart the terminal and paste 'conda config --set auto_activate_base false' to stop conda from activating base"
+
+# Installing Docker now :)
+read -p "Do you want to install Docker? {after installation the system will reboot !!!}(yes/no): " choice
+
+case "$choice" in
+  yes|Yes|YES)
+    apk update
+    apk add docker docker-compose
+    service docker start
+    rc-update add docker boot
+    addgroup $USER docker
+    reboot
+    ;;
+  no|No|NO)
+    echo "Skipping Docker installation."
+    ;;
+  *)
+    echo "Invalid choice, please enter 'yes' or 'no'."
+    ;;
+esac
